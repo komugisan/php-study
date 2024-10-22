622ce3f1b6a8f4bf6a858c961aca214815aa8770
ariant-dev <info@ariant.net>
2024-10-23 06:26:11 +0900

    1023

--- php/kadai-fizzbuzz.php
+++ php/kadai-fizzbuzz.php
@@ -9,19 +9,19 @@ function fizzbuzz() {
     for($i=1;$i<=$max;$i++){
         //数値が3の倍数であり5の倍数でもある場合
         if($i%3 === 0 && $i%5 === 0){
-            print_r("FizzBuzz") . PHP_EOL;
+            print("FizzBuzz") . PHP_EOL;
         }
         //数値が3の倍数
         else if($i%3 === 0){
-            print_r("Fizz") . PHP_EOL;
+            print("Fizz") . PHP_EOL;
         }
         //数値が5の倍数
         else if($i%5 === 0){
-            print_r("Buzz") . PHP_EOL;
+            print("Buzz") . PHP_EOL;
         }
         //その他はそのまま
         else{
-            print_r($i) . PHP_EOL;
+            print($i) . PHP_EOL;
         }
     }
 
